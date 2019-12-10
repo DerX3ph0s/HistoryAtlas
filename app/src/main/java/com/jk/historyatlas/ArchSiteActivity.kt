7b3e8a8db8c54df6be1dbee9fe17bbf92474e4d6
DerX3ph0s <jesper.koehler@gmail.com>
2019-12-10 18:52:25 +0100

    Initial commit

---
+++ app/src/main/java/com/jk/historyatlas/ArchSiteActivity.kt
@@ -0,0 +1,12 @@
+package com.jk.historyatlas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ArchSiteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_arch_site)
+    }
+}
