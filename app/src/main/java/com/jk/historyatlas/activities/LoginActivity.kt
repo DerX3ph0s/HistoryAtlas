cc7a1223cd4800ca84d01ce34717f3a9e85f7065
DerX3ph0s <jesper.koehler@gmail.com>
2020-01-08 15:00:00 +0100

    Release v0.1

--- app/src/main/java/com/jk/historyatlas/activities/LoginActivity.kt
+++ app/src/main/java/com/jk/historyatlas/activities/LoginActivity.kt
@@ -3,11 +3,16 @@ package com.jk.historyatlas.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import com.google.firebase.auth.FirebaseAuth
 import com.jk.historyatlas.R
 import kotlinx.android.synthetic.main.activity_login.*
 import org.jetbrains.anko.toast
 
 class LoginActivity : AppCompatActivity() {
+
+    var auth: FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,17 +41,43 @@ class LoginActivity : AppCompatActivity() {
                 doLogin(email,password)
             }
         }
+
+        progressBar.visibility = GONE
     }
 
     fun doSignUp (email: String, password: String) {
-        val intent = Intent(this, ArchSiteListActivity::class.java)
-        startActivity(intent)
-        finish()
+        showProgress()
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                val intent = Intent(this, ArchSiteListActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                toast("Sign Up Failed: ${task.exception?.message}")
+            }
+            hideProgress()
+        }
     }
 
     fun doLogin (email: String, password: String) {
-        val intent = Intent(this, ArchSiteListActivity::class.java)
-        startActivity(intent)
-        finish()
+        showProgress()
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                val intent = Intent(this, ArchSiteListActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                toast("Sign Up Failed: ${task.exception?.message}")
+            }
+            hideProgress()
+        }
+    }
+
+    fun showProgress() {
+        progressBar.visibility = VISIBLE
+    }
+
+    fun hideProgress() {
+        progressBar.visibility = GONE
     }
 }
